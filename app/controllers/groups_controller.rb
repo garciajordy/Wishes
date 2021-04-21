1c92f78eeb21316cb415935f6776c9cff4e55a39
garciajordy <jordygarcia0119@gmail.com>
2021-04-21 15:34:49 +0300

    Add group model and controller

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+class GroupsController < ApplicationController
+  def index
+    @groups = Group.all.order(created_at: 'DESC')
+  end
+
+  def show
+    @group = Group.find(params[:id])
+    @wishes = @group.wishlists.order(created_at: :desc)
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.user_id = current_user.id
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    redirect_to group_path
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
+  end
+end
---
+++ app/models/group.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class Group < ApplicationRecord
+  belongs_to :user
+  has_many :wishlists
+  validates :name, presence: true
+end
