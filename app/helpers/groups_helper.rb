14bc9666e86ed1580d9db75b32b22b2fb39c92d4
garciajordy <jordygarcia0119@gmail.com>
2021-04-22 11:40:41 +0300

    Add logic to helpers

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,7 +1,11 @@
 module GroupsHelper
   def imageurl(_group)
-    # return group.icon unless group.icon.nil?
+    return group.icon unless group.icon.nil?
 
     'https://bitsofco.de/content/images/2018/12/broken-1.png'
+  end
+
+  def name(wish)
+    User.find(wish.author_id).name
   end
 end
